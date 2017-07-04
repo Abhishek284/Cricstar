39b588ef7483d45ba9f97d4ca8509e09f8fbb06a
Abhishek <abhishek@Admins-Mac-mini.local>
2017-07-04 16:44:09 +0530

    Adding My cric project

---
+++ app/src/main/java/com/example/abhishek/cricstar/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.abhishek.cricstar;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+
+/**
+ * Created by abhishek on 04/07/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+   public void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+
+   }
+
+
+}
